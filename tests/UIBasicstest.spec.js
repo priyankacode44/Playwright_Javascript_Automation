becc3a8101629ea69fdc4d3f9cc4888446b6438b
priyankacode44 <priyankaspte44@gmail.com>
2025-11-25 10:14:57 +0530

    Initial commit: add Playwright tests and notes

---
+++ tests/UIBasicstest.spec.js
@@ -0,0 +1,8 @@
+const {test} = require('@playwright/test');
+
+
+test('first playwright test',function()
+{
+//it is asynchronus---not wait for the current pange jump on next page---we use await
+
+});
